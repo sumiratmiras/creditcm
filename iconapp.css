c818cfdc91369f17fcb9cfea588f92e28ec3da04
sumiratmiras <33399261+sumiratmiras@users.noreply.github.com>
2017-11-12 03:09:19 +0700

    Add files via upload

---
+++ iconapp.css
@@ -0,0 +1,8 @@
+.icon-app span{background-image:url(&#39;https://rawgit.com/mastamvan/image/master/download.png&#39;)  }
+.icon-app span.coc {background-image:url(&#39;https://rawgit.com/mastamvan/image/master/coc.png&#39;);}
+.icon-app span.clash-royale {background-image:url(&#39;https://rawgit.com/mastamvan/image/master/clash-royale-icon.png&#39;)}
+.icon-app span.get-rich {background-image:url(&#39;https://rawgit.com/mastamvan/image/master/get-rich-icon.png&#39;)}
+.icon-app span.adobe-cc {background-image:url(&#39;https://assets.jalantikus.com/assets/cache/300/300/apps/2015/02/18/cc-icon.jpeg&#39;)}
+.icon-app span.opera-mini{background-image:url(&#39;https://assets.jalantikus.com/assets/cache/96/96/apps/2017/04/13/opera-icon.jpeg&#39;)}
+.icon-app span.uc-browser{background-image:url(&#39;https://assets.jalantikus.com/assets/cache/96/96/apps/2016/07/19/uc-for-pc-icon.png&#39;)}
+.icon-app span.view-ig{background-image:url(&#39;http://lh3.googleusercontent.com/YzGbxiRhq7cxanI4_Utc5UeIh4u9zn6PSH96c1-RrlWzybGmYpZLsOSFAi8uvam6h7Ww=w300-rw&#39;)}
